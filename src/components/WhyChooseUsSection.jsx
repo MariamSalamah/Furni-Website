7cee8dcc1caff809d65bc0766f4d87f8b37748f6
Mariam <mariam.mohamed581@gmail.com>
2026-04-29 21:32:17 +0300

    add why choose us section

--- src/components/WhyChooseUsSection.jsx
+++ src/components/WhyChooseUsSection.jsx
@@ -1,6 +1,5 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 
 export default function WhyChooseUsSection() {
@@ -10,16 +9,213 @@ export default function WhyChooseUsSection() {
                               <Grid container>
                                                                   
                               <Grid size={6} sx={{ ml:12}}>
-                                  <Typography sx={{ color:'#2F2F2F', width:"90%", mb:4}} variant='h4'>Crafted with excellent material.</Typography>
-                                  <Typography sx={{ color:'#7d7d7d', width:"100%", fontSize: 14 , mb:6}}>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique.</Typography>
-                                    <Button rounded variant="contained" sx={{backgroundColor:'#151515e7', color:'#f3f3f3', borderRadius: '20px'}}>Explore</Button>
-         
+                                  <Typography sx={{ color:'#2F2F2F', width:"90%", mb:2,fontWeight:'semi-bold'}} variant='h4'>Why Choose Us</Typography>
+                                  <Typography sx={{ color:'#7d7d7d', width:"90%", fontSize: 14 , mb:6}}>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique.</Typography>  
+
+                              <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }} >
+
+                                <Box>
+                                     {/* icon */}
+
+                                 <Box
+                                      sx={{
+                                        position: 'relative', 
+                                        width: 50,
+                                        height: 50,
+                                      }}
+                                    >
+                                      {/* circle */}
+                                      <Box
+                                        sx={{    width: 32,
+                                            height: 32,
+                                            borderRadius: '50%',
+                                            bgcolor: '#CBD5D1',
+                                            display: 'flex',
+                                            justifyContent: 'center',
+                                            alignItems: 'center',}}
+                                      />
+
+                                      {/* icon */}
+                                      <Box
+                                        component="img"
+                                        src="src\assets\truck.svg"
+                                        alt="Overlay"
+                                        sx={{
+                                          position: 'absolute', 
+                                          top: -12,
+                                          right: 24,
+                                          width: 48,
+                                          height: 48,
+                                          zIndex: 1,
+                                        }}
+                                      />
+                                    </Box>
+
+
+                                    {/* text */}
+                                  <Typography sx={{ color:'#2F2F2F', width:"90%", fontSize: 14, mb:1}} variant='h6'>Fast & Free Shipping</Typography>
+                                  <Typography sx={{ color:'#7d7d7d', width:"90%", fontSize: 14 , mb:6}}>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate.</Typography>  
+                                
+
+                                </Box>
+
+                                 <Box>
+                                     {/* icon */}
+
+                                 <Box
+                                      sx={{
+                                        position: 'relative', 
+                                        width: 50,
+                                        height: 50,
+                                      }}
+                                    >
+                                      {/* circle */}
+                                      <Box
+                                        sx={{    width: 32,
+                                            height: 32,
+                                            borderRadius: '50%',
+                                            bgcolor: '#CBD5D1',
+                                            display: 'flex',
+                                            justifyContent: 'center',
+                                            alignItems: 'center',}}
+                                      />
+
+                                      {/* icon */}
+                                      <Box
+                                        component="img"
+                                        src="src\assets\bag.svg"
+                                        alt="Overlay"
+                                        sx={{
+                                          position: 'absolute', 
+                                          top: -12,
+                                          right: 24,
+                                          width: 48,
+                                          height: 48,
+                                          zIndex: 1,
+                                        }}
+                                      />
+                                    </Box>
+
+
+                                    {/* text */}
+                                  <Typography sx={{ color:'#2F2F2F', width:"90%", fontSize: 14, mb:1}} variant='h6'>Easy to Shop</Typography>
+                                  <Typography sx={{ color:'#7d7d7d', width:"90%", fontSize: 14 , mb:6}}>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate.</Typography>  
+                                
+
+                                </Box>
+                           
+
+
+                                </Box>
+
+
+
+
+
+
+                                <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }} >
+
+                                <Box>
+                                  {/* icon */}
+
+                                 <Box
+                                      sx={{
+                                        position: 'relative', 
+                                        width: 50,
+                                        height: 50,
+                                      }}
+                                    >
+                                      {/* circle */}
+                                      <Box
+                                        sx={{    width: 32,
+                                            height: 32,
+                                            borderRadius: '50%',
+                                            bgcolor: '#CBD5D1',
+                                            display: 'flex',
+                                            justifyContent: 'center',
+                                            alignItems: 'center',}}
+                                      />
+
+                                      {/* icon */}
+                                      <Box
+                                        component="img"
+                                        src="src\assets\support.svg"
+                                        alt="Overlay"
+                                        sx={{
+                                          position: 'absolute', 
+                                          top: -12,
+                                          right: 24,
+                                          width: 48,
+                                          height: 48,
+                                          zIndex: 1,
+                                        }}
+                                      />
+                                    </Box>
+
+
+                                    {/* text */}
+                                  <Typography sx={{ color:'#2F2F2F', width:"90%", fontSize: 14, mb:1}} variant='h6'>24/7 Support</Typography>
+                                  <Typography sx={{ color:'#7d7d7d', width:"90%", fontSize: 14 , mb:6}}>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate.</Typography>  
+                                
+
+                                </Box>
+
+                                 <Box>
+                                     {/* icon */}
+
+                                 <Box
+                                      sx={{
+                                        position: 'relative', 
+                                        width: 50,
+                                        height: 50,
+                                      }}
+                                    >
+                                      {/* circle */}
+                                      <Box
+                                        sx={{    width: 32,
+                                            height: 32,
+                                            borderRadius: '50%',
+                                            bgcolor: '#CBD5D1',
+                                            display: 'flex',
+                                            justifyContent: 'center',
+                                            alignItems: 'center',}}
+                                      />
+
+                                      {/* icon */}
+                                      <Box
+                                        component="img"
+                                        src="src\assets\return.svg"
+                                        alt="Overlay"
+                                        sx={{
+                                          position: 'absolute', 
+                                          top: -12,
+                                          right: 24,
+                                          width: 48,
+                                          height: 48,
+                                          zIndex: 1,
+                                        }}
+                                      />
+                                    </Box>
+
+
+                                    {/* text */}
+                                  <Typography sx={{ color:'#2F2F2F', width:"90%", fontSize: 14, mb:1}} variant='h6'>Hassle Free Returns</Typography>
+                                  <Typography sx={{ color:'#7d7d7d', width:"90%", fontSize: 14 , mb:6}}>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate.</Typography>  
+                                
+
+                                </Box>
+                           
+
+
+                                </Box>
+
+
                               </Grid>
 
 
                                      
                               <Grid size={5}>
-                              <Box component="img" src="src\assets\why-choose-us-img.jpg" sx={{width:"100%"}}/>              
+                              <Box component="img" src="src\assets\why-choose-us-img.jpg" sx={{width:"90%", borderRadius:8}}/>              
                               </Grid>
                   
                               </Grid>
